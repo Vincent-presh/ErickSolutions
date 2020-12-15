13e600babc26b451f54de47141b12cfbed3ed913
Vincent <5tcentunary@email.com>
2020-12-15 23:57:51 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+<script>
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+      apiKey: "AIzaSyCZ8YSXecz3vftPBtQeqYNdtz7cyvS5sco",
+      authDomain: "erick-solutions.firebaseapp.com",
+      databaseURL: "https://erick-solutions-default-rtdb.firebaseio.com",
+      projectId: "erick-solutions",
+      storageBucket: "erick-solutions.appspot.com",
+      messagingSenderId: "629049235964",
+      appId: "1:629049235964:web:8854fd86bbee6f1f91fa79",
+      measurementId: "G-SP53W03ZW1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();+
+  </script>
